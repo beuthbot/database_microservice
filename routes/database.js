c7a41f1a406792d4a7fe201b5f6adf065a2bcbf6
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2020-07-08 23:21:03 +0200

    implement set, delete, and get all logic

--- routes/database.js
+++ routes/database.js
@@ -1,16 +1,38 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const resolver = require('../services/intent-resolve')
+const util = require('util')
 
 router.post('/', function (req, res, next) {
-  resolver.resolve(req.body)
-    .then(data => {
-      const answer = data.error ? data.error : data.data
-      res.send(answer)
-    })
-    .catch(exception => {
-      res.send(exception)
-    })
+
+    const message = req.body
+
+    if (!message) {
+        return createDefaultErrorAnswer("no message given")
+    }
+
+    resolver.resolve(req.body)
+        .then(data => {
+            console.debug("data:\n" + util.inspect(data, false, null, true))
+            const answer = data.error ? data.error : data.data
+            res.json(data)
+            res.end()
+        })
+        .catch(exception => {
+            res.send(exception)
+        })
 });
+
+function createDefaultErrorAnswer(error) {
+    return new Promise((resolve) => {
+        resolve({
+            error: error,
+            answer: {
+                content: "Das kann ich irgendwie nicht.",
+                history: ["intent-resolve"]
+            }
+        })
+    })
+}
 
 module.exports = router;
--- services/intent-resolve.js
+++ services/intent-resolve.js
@@ -1,6 +1,8 @@
 const axios = require('axios')
 require('dotenv').config()
 
+const util = require('util')
+
 const url = process.env.DATABASE_ENDPOINT
 
 /**
@@ -8,18 +10,37 @@ const url = process.env.DATABASE_ENDPOINT
  * for example: if the intent name is 'database-get', the database operation GET will be kicked off
  * @param {the whole request body} intent 
  */
-function resolve(intent) {
-    switch (intent.intent.name) {
+async function resolve(intent) {
+
+    console.debug("resolve:\n" + util.inspect(intent, false, null, true))
+
+    let message = intent.message
+    if (!message) {
+        return createDefaultErrorAnswer("no message found")
+    }
+
+    if (!message.intent) {
+        return createDefaultErrorAnswer("no intent found")
+    }
+
+    if (!message.intent.name) {
+        return createDefaultErrorAnswer("intent has no name")
+    }
+
+    console.debug("intent-name: " + message.intent.name)
+
+    switch (message.intent.name) {
         case 'database-get':
-            return databaseGet(intent);
+            return databaseGet(message);
         case 'database-set':
-            return databaseSet(intent);
+            return databaseSet(message);
         case 'database-remove':
-            return databaseRemove(intent);
+            return databaseRemove(message);
         default:
-            return new Promise((resolve) => {
-                resolve({ error: `Database Operation '${intent.intent.name}' does not exist` })
-            })
+            return createErrorAnswer(
+                "Das kann ich nicht.",
+                `Database Operation '${intent.intent.name}' does not exist`
+            )
     }
 }
 
@@ -27,44 +48,228 @@ function resolve(intent) {
  * GET operation on the database via REST to retrieve the details of a user
  * @param {the request body piped from the resolve function} data 
  */
-function databaseGet(data) {
-    const userTelegramID = data['user']['telegram-id']
-    //const query = data['entities'][0]['value']
-    //const queryUrl = query == 'alles' ? `${url}/${userTelegramID}/detail` : `${url}/${userTelegramID}/detail?q=${query}`
+async function databaseGet(message) {
+    const beuthBotID = getBeuthBotID(message)
+    console.debug("beuthBotID: " + beuthBotID)
 
-    return axios.get(`${url}/${userTelegramID}/detail`)
+    let userDetailsResponse = await axios.get(`${url}/users/${beuthBotID}/detail`)
+    let userDetails = userDetailsResponse.data
+
+    console.debug("userDetails:\n" + util.inspect(userDetails, false, null, true))
+
+    if (userDetails) {
+
+        let userDetailsString = "Deine Daten:\n\n"
+
+        if (userDetails.nickname) { userDetailsString += "Nickname: **" + userDetails.nickname + "**\n" }
+        if (userDetails.firstName) { userDetailsString += "Vorname: **" + userDetails.firstName + "**\n" }
+        if (userDetails.lastName) { userDetailsString += "Nachname: **" + userDetails.lastName + "**\n" }
+        userDetailsString += "\n"
+
+        if (userDetails.details) {
+            for (const [ key, value ] of Object.entries(userDetails.details)) {
+                userDetailsString += String(key) + ": **" + value + "**\n"
+            }
+        }
+
+        return createAnswer(userDetailsString)
+    } else {
+        return createErrorAnswer(
+            "Das kann ich nicht finden.",
+            `No details for user with id: '${beuthBotID}'`
+        )
+    }
 }
 
 /**
- * The Database Set operation to set the user Details. Takes only the first entity from the entities array since it has the highes confidence score 
- * granted by rasa
+ * The Database Set operation to set the user Details. Takes only the first entity from the entities array since it has
+ * the highes confidence score granted by rasa
  * @param {the request body piped from the resolve function} data 
  */
-function databaseSet(data) {
-    const userTelegramID = data['user']['telegram-id']
+async function databaseSet(message) {
 
-    return axios.post(`${url}/${userTelegramID}/detail`, {
-        detail: data['entities'][0]['entity'],
-        value: data['entities'][0]['value']
+    const beuthBotID = getBeuthBotID(message)
+    if (!beuthBotID) {
+        return createDefaultErrorAnswer("can't find user id in message")
+    }
+
+    const detailEntity = findDetailsEntity(message)
+    if (!detailEntity) {
+        return createDefaultErrorAnswer("can't find detail entity")
+    }
+
+    let detailName = detailEntity.entity
+    if (!detailName) {
+        return createDefaultErrorAnswer("can't find detail name")
+    }
+
+    if (detailName === "all-details") {
+        return createDefaultErrorAnswer("can't set all-details")
+    }
+
+
+    // the name of the entity will always have "detail-" prefix
+    detailName = detailName.replace("detail-", "")
+    console.debug("detailName: " + detailName)
+
+    var detailValueEntity = null
+    switch (detailName) {
+        case 'home':
+            detailValueEntity = findEntity(message, "city")
+            break
+        case 'birthday':
+            detailValueEntity = findEntity(message, "time")
+            break
+        case 'meal-preference':
+            detailValueEntity = findEntity(message, "meal-preference")
+            break
+        case 'allergic':
+            detailValueEntity = findEntity(message, "allergen")
+            break
+        default:
+            detailValueEntity = findEntity(message, detailName)
+            break
+    }
+
+    if (!detailValueEntity) {
+        return createErrorAnswer(
+            `Ich habe kein Detail für '${detailName}' in der Nachricht gefunden.`,
+            `unknown detail name: '${detailName}'`
+        )
+    }
+
+    const detailValue = detailValueEntity.value
+    if (!detailValue) {
+        return createErrorAnswer(
+            `Ich habe kein Detail für '${detailName}' in der Nachricht gefunden.`,
+            `unknown detail name: '${detailName}'`
+        )
+    }
+
+    console.debug("detailValueEntity: " + detailValueEntity)
+    console.debug("detailValue: " + detailValue)
+
+    let databseResponse = await axios.post(`${url}/users/${beuthBotID}/detail`, {
+        detail: detailName,
+        value: detailValue
+    })
+
+    if (!databseResponse) {
+        return createDefaultErrorAnswer("no response from database")
+    }
+
+    console.debug("databseResponse: " + util.inspect(databseResponse, false, null, true))
+
+    return createAnswer("Das habe ich mir gemerkt.")
+}
+
+/**
+ * The Database Remove operation to remove Details of a user. Takes only the first entity from the entities array since
+ * it has the highes confidence score granted by rasa. If the entity 'alles' all the details of a user will be deleted
+ * @param {the request body piped from the resolve function} data 
+ */
+async function databaseRemove(message) {
+
+    const beuthBotID = getBeuthBotID(message)
+    if (!beuthBotID) {
+        return createDefaultErrorAnswer("can't find user id in message")
+    }
+
+    const detailEntity = findDetailsEntity(message)
+    if (!detailEntity) {
+        return createDefaultErrorAnswer("can't find detail entity")
+    }
+
+    let detailName = detailEntity.entity
+    if (!detailName) {
+        return createDefaultErrorAnswer("can't find detail name")
+    }
+
+    if (detailName === "all-details") {
+        return axios.delete(`${url}/users/${beuthBotID}/detail`)
+    }
+
+    // the name of the entity will always have "detail-" prefix
+    detailName = detailName.replace("detail-", "")
+    console.debug("detailName: " + detailName)
+
+    const databaseResponse = axios.delete(`${url}/users/${beuthBotID}/detail?q=${detailName}`)
+
+    if (!databaseResponse) {
+        return createDefaultErrorAnswer("no response from database")
+    }
+
+    console.debug("databseResponse: " + util.inspect(databaseResponse, false, null, true))
+
+    return createAnswer(`Ich habe das Detail '${detailName}' gelöscht.`)
+
+}
+
+function createAnswer(text) {
+    return new Promise((resolve) => {
+        resolve({
+            answer: {
+                content: text,
+                history: ["intent-resolve"]
+            }
+        })
     })
 }
 
-/**
- * The Database Remove operation to remove Details of a user. Takes only the first entity from the entities array since it has the highes confidence score 
- * granted by rasa. If the entity 'alles' all the details of a user will be deleted
- * @param {the request body piped from the resolve function} data 
- */
-function databaseRemove(data) {
-    const userTelegramID = data['user']['telegram-id']
+function createErrorAnswer(text, error) {
+    console.debug("createErrorAnswer: " + error)
+    return new Promise((resolve) => {
+        resolve({
+            error: error,
+            answer: {
+                content: text,
+                history: ["intent-resolve"]
+            }
+        })
+    })
+}
 
-    const query = data['entities'][0]['value']
+function createDefaultErrorAnswer(error) {
+    console.debug("createDefaultErrorAnswer: " + error)
+    return new Promise((resolve) => {
+        resolve({
+            error: error,
+            answer: {
+                content: "Das kann ich irgendwie nicht.",
+                history: ["intent-resolve"]
+            }
+        })
+    })
+}
 
-    const queryUrl = query == 'alles' ? `${url}/${userTelegramID}/detail` : `${url}/${userTelegramID}/detail?q=${query}`
+function getBeuthBotID(message) {
+    if (!message || !message.user || !message.user.id) { return null }
+    return message.user.id
+}
 
-    console.log(queryUrl)
+function findEntity(message, name) {
+    if (!message.entities) { return null }
+    if (!message.entities.length) { return null }
+    for (let i = 0; i < message.entities.length; i++) {
+        let entity = message.entities[i]
+        if (entity.entity === name) { return entity }
+    }
+}
 
-
-    return axios.delete(queryUrl)
+function findDetailsEntity(message) {
+    if (!message.entities) { return null }
+    if (!message.entities.length) { return null }
+    for (let i = 0; i < message.entities.length; i++) {
+        let entity = message.entities[i]
+        let entityName = entity.entity
+        if (entityName) {
+            if (entityName.startsWith("detail")) {
+                return entity
+            } else if (entity === "all-details") {
+                return entity
+            }
+        }
+    }
 }
 
 /**
